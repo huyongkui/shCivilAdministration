ddf3f67cac951ad541ff2dbd969017b2edfb014f
wangbolun <583288277@qq.com>
2016-09-21 18:47:48 +0800

    temp

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(function($){
+	var swiper = new Swiper('.index_carousel_wrapper', {
+		loop: true,
+		pagination: '.swiper-pagination',
+		nextButton: '.arrow_right',
+		prevButton: '.arrow_left',
+		paginationClickable: true
+	});
+})(jQuery)
