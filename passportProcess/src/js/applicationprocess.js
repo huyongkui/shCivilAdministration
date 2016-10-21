fc5c28f1fbbb8533d436037e28e54e19dcc39f1d
huyongkui <1015210516@qq.com>
2016-10-21 14:35:11 +0800

    shenqingbiaoge

--- passportProcess/src/js/applicationprocess.js
+++ passportProcess/src/js/applicationprocess.js
@@ -4,8 +4,6 @@ $(function(){
 		console.log($('#'+$(this).attr('name')))
 		$('.leftarea #'+$(this).attr('name')).show().siblings(".area").hide();
 	})
-//	$('.hid').hide();
-	$('.huzhao').val('2').click(function(){
-		
-	});
+
+	
 })
