6912b96b9aeabb25ee7b22d9e46cc4604d2ee8bc
huyongkui <1015210516@qq.com>
2016-10-21 13:24:33 +0800

    tianbiao

--- passportProcess/src/js/applicationprocess.js
+++ passportProcess/src/js/applicationprocess.js
@@ -4,4 +4,8 @@ $(function(){
 		console.log($('#'+$(this).attr('name')))
 		$('.leftarea #'+$(this).attr('name')).show().siblings(".area").hide();
 	})
+//	$('.hid').hide();
+	$('.huzhao').val('2').click(function(){
+		
+	});
 })
